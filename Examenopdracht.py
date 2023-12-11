052164b78fb0e2c80e42ac3a97348852eaf32c76
Kanker-Klop <150671397+Kanker-Klop@users.noreply.github.com>
2023-12-11 12:20:21 +0100

    Initialisatie voor te testen of alles nog werkt

---
+++ Examenopdracht.py
@@ -0,0 +1,11 @@
+import apgiotboard
+from time import sleep
+import random
+
+apgiotboard.all_leds_off()
+
+while True:
+    apgiotboard.led(1).on()
+    sleep(0.05)
+    apgiotboard.led(1).off()
+    sleep(0.05)
