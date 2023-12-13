7ec9de0ea40859f45e3060d6675a224bf4172b94
Kanker-Klop <150671397+Kanker-Klop@users.noreply.github.com>
2023-12-13 12:02:26 +0100

    Programma 2 klaar, start van programma 3
    Programma 2 doet het volgende:
    De stand van de potentiometer wordt gelezen. Vervolgens wordt de overeenkomstige pin (en wederom LED voor na te gaan of dit programma wel werkt) aangezet.
    De aangestuurde hardware zullen een paar (8) oscillators aansturen, die ingesteld staan als een muzikale toonladder.

--- Examenopdracht.py
+++ Examenopdracht.py
@@ -20,8 +20,25 @@ while umoeder == '':
             program1 = "VOLGENDES"
             program2 = "flak"
     while program2 == "flak":
-        posipinne = apgiotboard.potentiometer(high = in range (13,22))
-        posiledde = apgiotboard.potentiometer(apgiotboard.MAXLED)
+        vorigekneker = None
+        vorigeposipinne = None
+        posipinne = round(apgiotboard.potentiometer(7))+14
+        kneker = round(apgiotboard.potentiometer(apgiotboard.MAXLED))
+        apgiotboard.led(kneker).on()
+        if vorigekneker != kneker:
+            vorigekneker = kneker
+            apgiotboard.all_leds_off()
+        Pin(posipinne, Pin.OUT).on()
+        if vorigeposipinne != posipinne:
+            vorigeposipinne = posipinne
+            for crack in range(14,22):
+                Pin(crack, Pin.OUT).off()
+        sleep(0.0000001)
+        if apgiotboard.button_pressed(1):
+            program2 = "VOLGENDES"
+            program3 = "flak"
+    while program3 =: "flak":
+            
 
         
     
