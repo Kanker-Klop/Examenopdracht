981df013e09cfa1b0c5a6be8282a82d692144cf2
Kanker-Klop <150671397+Kanker-Klop@users.noreply.github.com>
2023-12-13 11:44:57 +0100

    Eerste programma klaar; alleen nog hardware nodig
    Heel simpel: wanneer op knop 1 wordt geduwt, gaat pin 13 aan en ook LED 1 (LED 1 voor te checken of het programma daadwerkelijk werkt)
    De hardware wordt het volgende: een transistor als schakelaar die een relais aanstuurt, die een lamp laat branden.

--- Examenopdracht.py
+++ Examenopdracht.py
@@ -3,9 +3,23 @@ from time import sleep
 import random
 from machine import Pin
 
+apgiotboard.all_leds_off()
+
 umoeder = ''
 program1 = "flak"
 while umoeder == '':
-    while program1 = "flak":
+    while program1 == "flak":
         if apgiotboard.button_pressed(0):
-            
+            Pin(13, Pin.OUT).on()
+            apgiotboard.led(1).on()
+        else:
+            Pin(13, Pin.OUT).off()
+            apgiotboard.led(1).off()
+        sleep(0.01)
+        if apgiotboard.button_pressed(1):
+            program1 = "VOLGENDES"
+            program2 = "flak"
+    while program2 == "flak":
+
+        
+    
