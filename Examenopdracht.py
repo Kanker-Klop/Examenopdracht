35204b17aafc6a024c44dd1a876506dd6534032b
Kanker-Klop <150671397+Kanker-Klop@users.noreply.github.com>
2023-12-13 11:32:19 +0100

    Als knop 1 wordt ingedrukt, dan:

--- Examenopdracht.py
+++ Examenopdracht.py
@@ -1,9 +1,11 @@
 import apgiotboard
 from time import sleep
 import random
+from machine import Pin
 
 umoeder = ''
 program1 = "flak"
 while umoeder == '':
-    while program1 = "flak"
-    
+    while program1 = "flak":
+        if apgiotboard.button_pressed(0):
+            
