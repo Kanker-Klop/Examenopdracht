83a1fab732b14861754dcddc5d714615e4347400
Kanker-Klop <150671397+Kanker-Klop@users.noreply.github.com>
2023-12-20 12:34:33 +0100

    Alle variabelen en functies een fatsoenlijke naam gegeven, en misschien nog wat layout verfatsoenlijkt

--- Examenopdracht.py
+++ Examenopdracht.py
@@ -5,10 +5,10 @@ from machine import Pin
 
 apgiotboard.all_leds_off()
 
-umoeder = ''
-program1 = "flak"
-while umoeder == '':
-    while program1 == "flak":
+bestaan = ''
+program1 = "AAN"
+while bestaan == '':
+    while program1 == "AAN":
         if apgiotboard.button_pressed(0):
             Pin(13, Pin.OUT).on()
             apgiotboard.led(1).on()
@@ -18,78 +18,79 @@ while umoeder == '':
         sleep(0.01)
         if apgiotboard.button_pressed(1,True):
             program1 = "VOLGENDES"
-            program2 = "flak"
-    while program2 == "flak":
-        vorigekneker = None
+            program2 = "AAN"
+    while program2 == "AAN":
+        vorigeledje = None
         vorigeposipinne = None
         posipinne = round(apgiotboard.potentiometer(7))+14
-        kneker = round(apgiotboard.potentiometer(apgiotboard.MAXLED))
-        apgiotboard.led(kneker).on()
-        if vorigekneker != kneker:
-            vorigekneker = kneker
+        ledje = round(apgiotboard.potentiometer(apgiotboard.MAXLED))
+        apgiotboard.led(ledje).on()
+        if vorigeledje != ledje:
+            vorigeledje = ledje
             apgiotboard.all_leds_off()
         Pin(posipinne, Pin.OUT).off()
         if vorigeposipinne != posipinne:
             vorigeposipinne = posipinne
-            for crack in range(14,22):
-                Pin(crack, Pin.OUT).on()
+            for draaien2 in range(14,22):
+                Pin(draaien2, Pin.OUT).on()
         if apgiotboard.button_pressed(1,True):
             program2 = "VOLGENDES"
-            program3 = "flak"
-    while program3 == "flak":
-        nentemp = apgiotboard.tempsensor()
+            program3 = "AAN"
+    while program3 == "AAN":
+        detemp = apgiotboard.tempsensor()
         ambtemp = round(apgiotboard.potentiometer(16)+19)
-        if nentemp > ambtemp:
-            leddenhoogte = (nentemp % ambtemp)
-            for temphoogte in range(1,leddenhoogte+1):
+        if detemp > ambtemp:
+            LEDshoogte = (detemp % ambtemp)
+            for temphoogte in range(1,LEDshoogte+1):
                 apgiotboard.led(temphoogte).on()
             sleep(0.00001)
             apgiotboard.all_leds_off()
         sleep(0.000001)
         if apgiotboard.button_pressed(1,True):
             program3 = "VOLGENDES"
-            program4 = "flak"
-    while program4 == "flak":
-        vorigeaids = 0
-        vorigeaids2 = 0
-        vorigeaids3 = 0
+            program4 = "AAN"
+    while program4 == "AAN":
+        vorigewillekeur = 0
+        vorigewillekeur2 = 0
+        vorigewillekeur3 = 0
         while True:
-            apgiotboard.led(vorigeaids).off()
-            apgiotboard.led(vorigeaids2).off()
-            apgiotboard.led(vorigeaids3).off()
-            aids = random.randint(1,apgiotboard.MAXLED)
-            aids2 = random.randint(1,apgiotboard.MAXLED)
-            aids3 = random.randint(1,apgiotboard.MAXLED)
-            if aids == aids2 == aids3:
-                for kakaidskak in range(1,apgiotboard.MAXLED+1):
-                    apgiotboard.led(kakaidskak).on()
+            apgiotboard.led(vorigewillekeur).off()
+            apgiotboard.led(vorigewillekeur2).off()
+            apgiotboard.led(vorigewillekeur3).off()
+            willekeur = random.randint(1,apgiotboard.MAXLED)
+            willekeur2 = random.randint(1,apgiotboard.MAXLED)
+            willekeur3 = random.randint(1,apgiotboard.MAXLED)
+            if willekeur == willekeur2 == willekeur3:
+                for kakwillekeurkak in range(1,apgiotboard.MAXLED+1):
+                    apgiotboard.led(kakwillekeurkak).on()
                     sleep(0.2)
-
             else:
-                apgiotboard.led(aids).on()
-                apgiotboard.led(aids2).on()
-                apgiotboard.led(aids3).on()
-            vorigeaids = aids
-            vorigeaids2 = aids2
-            vorigeaids3 = aids3
-            tijdgat = apgiotboard.potentiometer(0.8) 
-            sleep(tijdgat)
+                apgiotboard.led(willekeur).on()
+                apgiotboard.led(willekeur2).on()
+                apgiotboard.led(willekeur3).on()
+            vorigewillekeur = willekeur
+            vorigewillekeur2 = willekeur2
+            vorigewillekeur3 = willekeur3
+            tijd = apgiotboard.potentiometer(0.8) 
+            sleep(tijd)
             if apgiotboard.button_pressed(1,True):
                 program4 = "VOLGENDES"
-                program5 = "flak"
+                program5 = "AAN"
                 apgiotboard.all_leds_off()
                 break
-    while program5 == "flak":
+    while program5 == "AAN":
         def willekeurige_meneeren():
-                kaka1 = 2+random.randint(0,4)
-                kaka2 = 5+random.randint(0,1)
-                kakpis = (kaka1 == kaka2)
-                return kakpis
+           # """dus basically, deze functie maakt 2 willekeurige variabelen (met offset), en als deze gelijk zijn is de returnwaarde True;
+#anders is deze False"""
+                willeke1 = 2+random.randint(0,4)
+                willeke2 = 5+random.randint(0,1)
+                gelijkheid = (willeke1 == willeke2)
+                return gelijkheid
         apgiotboard.all_leds_off()
         if apgiotboard.button_pressed(0):
-            kakaids = willekeurige_meneeren()
-            print(kakaids)
-            if kakaids:
+            levenstoestand = willekeurige_meneeren()
+            print(levenstoestand)
+            if levenstoestand:
                 for doodgaan in range(1,13):
                     for DOOD in range(1, apgiotboard.MAXLED+1):
                         apgiotboard.led(DOOD).on()
@@ -102,4 +103,4 @@ while umoeder == '':
                 sleep(1)
         if apgiotboard.button_pressed(1,True):
             program5 = "VOLGENDES"
-            program1 = "flak"
+            program1 = "AAN"
